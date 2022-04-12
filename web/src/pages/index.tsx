28decb05eb2a7d30331ffb4e7e87e15d5a4bc6a2
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2022-04-11 22:43:15 -0300

    setup graphql client and tailwindcss

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { GetServerSideProps } from 'next';
-import { getAccessToken, getSession } from '@auth0/nextjs-auth0';
+import { getSession } from '@auth0/nextjs-auth0';
 
 export default function Auth() {
 	return null;
@@ -7,10 +7,6 @@ export default function Auth() {
 
 export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
 	const session = getSession(req, res);
-
-	const token = getAccessToken(req, res);
-
-	console.log(token)
 
 	if (!session) {
 		return {
